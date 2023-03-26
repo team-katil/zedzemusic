b74a0c5977baed29059c8b185991c748c7759547
team-katil <115205040+team-katil@users.noreply.github.com>
2023-03-26 15:15:30 +0530

    Update misc.py

--- ZedzeX/misc.py
+++ ZedzeX/misc.py
@@ -49,22 +49,22 @@ def sudo():
         for user_id in OWNER:
             SUDOERS.add(user_id)
     else:
-    sudoersdb = pymongodb.sudoers
-    sudoers = sudoersdb.find_one({"sudo": "sudo"})
-    sudoers = [] if not sudoers else sudoers["sudoers"]
-    for user_id in OWNER:
-        SUDOERS.add(user_id)
-        if user_id not in sudoers:
-            sudoers.append(user_id)
-            sudoersdb.update_one(
-                {"sudo": "sudo"},
-                {"$set": {"sudoers": sudoers}},
-                upsert=True,
-            )
-    if sudoers:
-        for x in sudoers:
-            SUDOERS.add(x)
-    LOGGER(__name__).info(f"Sudo Users Loaded Successfully.")
+        sudoersdb = pymongodb.sudoers
+        sudoers = sudoersdb.find_one({"sudo": "sudo"})
+        sudoers = [] if not sudoers else sudoers["sudoers"]
+        for user_id in OWNER:
+            SUDOERS.add(user_id)
+            if user_id not in sudoers:
+                sudoers.append(user_id)
+                sudoersdb.update_one(
+                    {"sudo": "sudo"},
+                    {"$set": {"sudoers": sudoers}},
+                    upsert=True,
+                )
+        if sudoers:
+            for x in sudoers:
+                SUDOERS.add(x)
+    LOGGER(__name__).info(f"Sudoers Loaded.")
 
 
 def heroku():
