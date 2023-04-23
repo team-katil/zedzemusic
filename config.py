faf7e602c921889abb4eb3886f478883702e81f8
team-katil <115205040+team-katil@users.noreply.github.com>
2023-04-23 17:10:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ MONGO_DB_URI = getenv("MONGO_DB_URI", None)
 LOG_GROUP_ID = int(getenv("LOG_GROUP_ID"))
 MUSIC_BOT_NAME = getenv("MUSIC_BOT_NAME", "Zᴇᴅᴢᴇ ✘")
 
-OWNER_ID = list(map(int, getenv("OWNER_ID", "5604140011").split()))
+OWNER_ID = list(map(int, getenv("OWNER_ID", "5301800943").split()))
 
 HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
