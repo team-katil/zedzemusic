d222020e23432143e0102e9bfbebf488484502d6
team-katil <115205040+team-katil@users.noreply.github.com>
2023-05-08 09:58:06 +0530

    Update tagall.py

--- ZedzeX/plugins/tagall.py
+++ ZedzeX/plugins/tagall.py
@@ -178,13 +178,13 @@ async def mentionall(client, message):
         usrnum += 1
         usrtxt += f"[{usr.user.first_name}](tg://user?id={usr.user.id}) "
 
-        if usrnum == 1:
+        if usrnum == 5:
             if mode == "text_on_cmd":
                 txt = f"{usrtxt} {random.choice(TAGSRT)}"
                 await client.send_message(chat_id, txt)
             elif mode == "text_on_reply":
                 await msg.reply(f"[{random.choice(EMOJI)}](tg://user?id={usr.user.id})")
-            await asyncio.sleep(3)
+            await asyncio.sleep(5)
             usrnum = 0
             usrtxt = ""
     try:
