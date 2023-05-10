dc1d56395701bb17a3b29dddf101aa17e77f1fef
team-katil <115205040+team-katil@users.noreply.github.com>
2023-05-10 19:23:08 +0530

    Update help.py

--- ZedzeX/plugins/help.py
+++ ZedzeX/plugins/help.py
@@ -1,12 +1,3 @@
-#
-# Copyright (C) 2021-2022 by TeamYukki@Github, < https://github.com/TeamYukki >.
-#
-# This file is part of < https://github.com/TeamYukki/YukkiMusicBot > project,
-# and is released under the "GNU v3.0 License Agreement".
-# Please see < https://github.com/TeamYukki/YukkiMusicBot/blob/master/LICENSE >
-#
-# All rights reserved.
-
 
 from typing import Union
 
@@ -70,7 +61,6 @@ async def helper_private(
         language = await get_lang(chat_id)
         _ = get_string(language)
         keyboard = help_pannel(_)
-        await update.reply_sticker("CAACAgUAAxkBAAIjVmKPYTFByKZlCo9d8mUv8QVAJEw7AAL9BQACiy14VGoQxOCDfE1KJAQ")
         await update.reply_photo(
             photo=config.START_IMG_URL,
             caption=_["help_1"], reply_markup=keyboard)
