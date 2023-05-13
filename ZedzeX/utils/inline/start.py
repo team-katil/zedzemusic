00123d89c55a3915325cef5f05b568b903426006
THE VIP BOY <117478195+THE-VIP-BOY-OP@users.noreply.github.com>
2023-05-13 12:30:47 +0530

    Update start.py

--- ZedzeX/utils/inline/start.py
+++ ZedzeX/utils/inline/start.py
@@ -44,7 +44,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="✯ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ✯", url=config.SUPPORT_GROUP
+                text="✯ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ✯", url=f"https://t.me/AlonesHeaven"
             ),
             InlineKeyboardButton(
                 text="✯ 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 ✯", url=f"https://t.me/AloneXBots",
