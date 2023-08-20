9d07021944a20b09ffbffd45a09484b0e854af93
team-katil <115205040+team-katil@users.noreply.github.com>
2023-08-20 18:40:02 +0530

    Update start.py

--- ZedzeX/utils/inline/start.py
+++ ZedzeX/utils/inline/start.py
@@ -1,8 +1,12 @@
 from typing import Union
-
+import re
+import os
+from os import getenv
+from dotenv import load_dotenv
+from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-
 import config
+from config import GROUP_USERNAME, CHANNEL_USERNAME
 
 
 def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
@@ -25,29 +29,31 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
      ]
     return buttons
 
+#extra shit
+BOT_USERNAME = ("{BOT_USERNAME}")
 
 def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
+    global GROUP_USERNAME
+    global CHANNEL_USERNAME
     buttons = [
         [
             InlineKeyboardButton(
                 text="✯ 𝗔𝗱𝗱 𝗠𝗲 𝗧𝗼 𝗬𝗼𝘂𝗿 𝗚𝗿𝗼𝘂𝗽 ✯",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
+            
         ],
         [
             InlineKeyboardButton(
-                text="✯ 𝗢𝘄𝗻𝗲𝗿 ✯", url=f"https://t.me/katil_your_dad"
-            ),
-            InlineKeyboardButton(
-                text="✯ 𝗛𝗲𝗹𝗽 ✯", callback_data="settings_back_helper"
+                text="۞ 𝐀𝙻𝙻 𝐅𝙴𝙰𝚃𝚄𝚁𝙴𝚂 ۞", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="✯ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ✯", url=config.SUPPORT_GROUP
+                text="✯ 𝗦𝘂𝗽𝗽𝗼𝗿𝘁 ✯", url=f"https://t.me/{GROUP_USERNAME}",
             ),
             InlineKeyboardButton(
-                text="✯ 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 ✯", url=f"https://t.me/katil_bots",
+                text="✯ 𝗨𝗽𝗱𝗮𝘁𝗲𝘀 ✯", url=f"https://t.me/{CHANNEL_USERNAME}",
             )
         ],
         [
