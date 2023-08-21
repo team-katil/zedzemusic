10e4034cadc06363681a839405bd4cd3e03dfcdd
team-katil <115205040+team-katil@users.noreply.github.com>
2023-08-21 20:41:51 +0530

    v2.0

--- strings/__init__.py
+++ strings/__init__.py
@@ -5,7 +5,7 @@ import yaml
 
 languages = {}
 commands = {}
-
+languages_present = {}
 
 def get_command(value: str) -> List:
     return commands["command"][value]
@@ -28,6 +28,7 @@ for filename in os.listdir(r"./strings/langs/"):
         languages["en"] = yaml.safe_load(
             open(r"./strings/langs/en.yml", encoding="utf8")
         )
+        languages_present["en"] = languages["en"]["name"])
     if filename.endswith(".yml"):
         language_name = filename[:-4]
         if language_name == "en":
@@ -38,3 +39,8 @@ for filename in os.listdir(r"./strings/langs/"):
         for item in languages["en"]:
             if item not in languages[language_name]:
                 languages[language_name][item] = languages["en"][item]
+    try:
+        languages_present[language_name] = languages[language_name]["name"]
+    except:
+        print("There is some issue with the language file inside bot.")
+        exit()
