3db31cb349a089b0dc24205568a194d26713db58
team-katil <115205040+team-katil@users.noreply.github.com>
2023-08-24 07:05:39 +0530

    v2.0

--- ZedzeX/plugins/__init__.py
+++ ZedzeX/plugins/__init__.py
@@ -3,10 +3,11 @@ from os.path import basename, dirname, isfile
 
 
 def __list_all_modules():
+    work_dir = dirname(__file__)
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
 
     all_modules = [
-        basename(f)[:-3]
+        (((f.replace(work_dir, "")).replace("/", "."))[:-3])
         for f in mod_paths
         if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
     ]
